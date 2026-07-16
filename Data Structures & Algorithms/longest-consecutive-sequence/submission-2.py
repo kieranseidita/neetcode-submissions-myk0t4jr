6f9f64dced396d640ee58b4eeffe2264a3d90f31
Kieran Seidita <seiditakieran@gmail.com>
2026-07-16 15:11:13 -0400

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+      #Step 1: We need to create 2 variables: a  hashset and then a max_consecutive variable
+      hash_set = set()
+      max_consec = 0
+
+      #Step 2: We need to now add all of our values to our hash_set
+      for num in nums:
+        hash_set.add(num)
+      
+      #Step 3: We now need a 2nd pass to check if its left neighbor is in the hash_set
+      for n in hash_set:
+        #Step 4: If n -1 isnt in hash_set we assume that we are starting a new sequence
+        if n - 1 not in hash_set:
+            #Step 5: Every start sequence 
+            start_consec = 1
+            #Step 6: If we have our right neighbor in the hash_set, then we will increment this by one
+            while n + start_consec in hash_set:
+                start_consec += 1
+            #Step 7: After going through the cosnecutive sequence, we will check if our max consecutive needs to be changed
+            max_consec = max(max_consec,start_consec)
+      
+      return max_consec
